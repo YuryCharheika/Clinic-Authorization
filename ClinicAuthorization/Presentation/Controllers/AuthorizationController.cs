e642d6d4b249b52bf5ed9e17be57e2d6bc137972
PiccoloPeccato <159267834+PiccoloPeccato@users.noreply.github.com>
2024-02-08 14:17:47 +0300

    Create project

---
+++ ClinicAuthorization/Presentation/Controllers/AuthorizationController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicAuthorization.Presentation.Controllers
+{
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        
+    }
+}
